1d533fdbf9d986b16b28892c758b0029a6f57d41
varun <varunsaini417@gmail.com>
2020-08-21 11:03:13 +0530

    Initial commit

---
+++ src/code/ja/Main.java
@@ -0,0 +1,30 @@
+package code.ja;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte fst = 50;
+        short second= 5000;
+        int third= 456465;
+        long finalsum=  (500000000000L+10L*(fst+second+third));
+        System.out.println(finalsum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
